35709ac99a5c62baa0e61e7e6c6848f43d563d81
shanqing-cai <shanqing.cai@gmail.com>
2013-11-18 11:20:22 -0500

    Added support for xh mode

--- write_netw_component_txt.m
+++ write_netw_component_txt.m
@@ -1,13 +1,26 @@
 function [nEdges, nNodes] = ...
-    write_netw_component_txt(adj, ncomp, rois, pvals, txtfn)
+    write_netw_component_txt(adj, ncomp, rois, pvals, txtfn, varargin)
+%% Process optional input arguments 
+bXH = ~isempty(fsic(varargin, '--xh'));
+
+%%
 txtf = fopen(txtfn, 'wt');
 
+if bXH && (size(pvals, 1) == size(adj, 1) / 2) ...
+       && (size(pvals, 2) == size(adj, 2) / 2)
+    pvals0 = pvals;
+    N0 = size(pvals, 1);
+    pvals = zeros(N0 * 2, N0 * 2);
+    pvals(1 : N0, N0 + 1 : 2 * N0) = pvals0;
+    pvals = pvals + pvals';
+end
 assert(isequal(size(adj), size(pvals)));
 
 idx = find(adj == ncomp);
 % nEdges = length(idx);
 
-N = numel(rois);
+% N = numel(rois);
+N = size(adj, 1);
 
 nEdges = 0;
 nNodes = 0;
@@ -29,14 +42,28 @@ for i1 = 1 : length(idx)
         t_pval = pvals(ix, iy);
     end
     
-    fprintf(txtf, '%s - %s: sig=%f\n', ...
-            rois{ix}, rois{iy}, -log10(t_pval));
+    if ~bXH    
+        fprintf(txtf, '%s - %s: sig=%f\n', ...
+                rois{ix}, rois{iy}, -log10(t_pval));
+    else
+        fprintf(txtf, '%s - %s: sig=%f\n', ...
+                sprintf('lh_%s', rois{ix}), ...
+                sprintf('rh_%s', rois{iy - N0}), ...
+                -log10(t_pval));
+    end
 
     if isempty(fsic(nodeList, rois{ix}))
         nodeList{end + 1} = rois{ix};
     end
-    if isempty(fsic(nodeList, rois{iy}))
-        nodeList{end + 1} = rois{iy};
+    
+    if ~bXH
+        iy1 = iy;
+    else
+        iy1 = iy - N0;
+    end
+    
+    if isempty(fsic(nodeList, rois{iy1}))
+        nodeList{end + 1} = rois{iy1};
     end
     
 	nEdges = nEdges + 1;
